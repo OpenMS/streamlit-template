756376ca1fbe380e7e1c33b3bb3f60f69bd421fd
Andrew Almaguer <andrew@almaguer.dev>
2023-05-09 20:51:39 +0200

    Add support for title and show_legend in updated vue-component submodule

--- src/components.py
+++ src/components.py
@@ -5,20 +5,22 @@ class PlotlyHeatmap:
     x = []
     y = []
     intensity = []
+    show_legend = None
 
     _plotly_heatmap = components.declare_component(
         "plotly_heatmap", 
         url="http://localhost:5173",
       )
 
-    def __init__(self, title, x, y, intensity):
+    def __init__(self, title, x, y, intensity, show_legend=False):
         self.title = title
         self.x = x
         self.y = y
         self.intensity = intensity
+        self.show_legend = show_legend
     
     def addComponent(self, key=None):
-        return self._plotly_heatmap(title=self.title, x=self.x, y=self.y, intensity=self.intensity, key=key)
+        return self._plotly_heatmap(title=self.title, x=self.x, y=self.y, intensity=self.intensity, show_legend=self.show_legend, key=key)
 
 
 
