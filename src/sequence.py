6a4e22ee2f8796faa2fe2be59d7f053d511b6887
JeeH-K <jeehyeong91@gmail.com>
2023-08-21 22:09:13 +0200

    receiving the fixed modification info at JS side - change the font color in fragment map

--- src/sequence.py
+++ src/sequence.py
@@ -44,49 +44,43 @@ def getFragmentMassesWithSeq(protein, res_type):
 
 
 def setFixedModification(protein):
-    fixed_mod_dict = {}
+    fixed_mod_site = []
 
     # fixed modification on cysteine
     if 'fixed_mod_cysteine' in st.session_state and st.session_state['fixed_mod_cysteine']:
         mod_mass = fixed_mod_cysteine[st.session_state['fixed_mod_cysteine']]
-        mod_indices = []
         for index, aa in enumerate(protein.toString()):
             if aa != 'C':
                 continue
             # to remove warning, setModificationByDiffMonoMass was not used.
             mod = ModificationsDB().getBestModificationByDiffMonoMass(mod_mass, 0.001, 'C', 0)
             protein.setModification(index, mod)
-            mod_indices.append(index)
-        if mod_indices:
-            fixed_mod_dict['C'] = mod_indices
+        fixed_mod_site.append('C')
 
     # fixed modification on methionine
     if 'fixed_mod_methionine' in st.session_state and st.session_state['fixed_mod_methionine']:
         mod_mass = fixed_mod_methionine[st.session_state['fixed_mod_methionine']]
-        mod_indices = []
         for index, aa in enumerate(protein.toUnmodifiedString()):
             if aa != 'M':
                 continue
                 # to remove warning, setModificationByDiffMonoMass was not used.
             mod = ModificationsDB().getBestModificationByDiffMonoMass(mod_mass, 0.001, 'M', 0)
             protein.setModification(index, mod)
-            mod_indices.append(index)
-        if mod_indices:
-            fixed_mod_dict['M'] = mod_indices
+        fixed_mod_site.append('M')
 
-    return protein, fixed_mod_dict
+    return protein, fixed_mod_site
 
 
-@st.cache_data
+# @st.cache_data
 def getFragmentDataFromSeq(sequence):
     protein = AASequence.fromString(sequence)
-    protein, fixed_mod_dict = setFixedModification(protein)  # handling fixed modifications
+    protein, fixed_mods = setFixedModification(protein)  # handling fixed modifications
 
     # calculating proteoform mass from sequence
     protein_mass = protein.getMonoWeight()
 
     out_object = {'sequence': list(sequence),
-                  'theoretical_mass': protein_mass, 'fixed_modifications': fixed_mod_dict}
+                  'theoretical_mass': protein_mass, 'fixed_modifications': fixed_mods}
     # per ion type, calculate the possible fragment masses and save them in dictionary
     for ion_type in ['ax', 'by', 'cz']:
         # calculate fragment ion masses
