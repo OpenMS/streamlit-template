2232c84dd1abbfa2583f51309b339d0fea3ba57f
JeeH-K <jeehyeong91@gmail.com>
2023-09-07 11:12:00 +0200

    FLASHQuant page is now redirected to JS

--- pages/FLASHQuantViewer.py
+++ pages/FLASHQuantViewer.py
@@ -1,11 +1,12 @@
-import streamlit as st
 from src.common import defaultPageSetup
+import streamlit.components.v1 as st_components
 
 
 def content():
-    defaultPageSetup()
-    st.title('FLASHQuant')
-    st.write(st.session_state['changed_tool_name'])
+    defaultPageSetup('FLASHQuant Viewer')
+    _flash_viewer_grid = st_components.declare_component("flash_viewer_grid",
+                                                         url="http://localhost:5173")
+    _flash_viewer_grid(components=[[{'componentArgs': {'componentName': 'FLASHQuantView'}}]])
 
 
 if __name__ == "__main__":
