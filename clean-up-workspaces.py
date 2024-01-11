8bc3a0a898f47017e2172bcf2db9caab09f7403e
JeeH-K <jeehyeong91@gmail.com>
2024-01-11 17:50:24 +0100

    Update clean-up-workspaces for FLASHViewer

--- clean-up-workspaces.py
+++ clean-up-workspaces.py
@@ -6,7 +6,7 @@ import shutil
 from datetime import datetime
 
 # Define the workspaces directory
-workspaces_directory = Path("/workspaces-streamlit-template")
+workspaces_directory = Path("/app/workspaces")
 
 # Get the current time in seconds
 current_time = time.time()
