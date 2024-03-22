6012ac7c0d8c7e06ceb79d21267ef6d854de70d7
Tom David Müller <57191390+t0mdavid-m@users.noreply.github.com>
2024-03-22 13:03:52 +0100

    Update hook-streamlit.py

--- hooks/hook-streamlit.py
+++ hooks/hook-streamlit.py
@@ -1,11 +1,8 @@
 from PyInstaller.utils.hooks import collect_data_files, copy_metadata
 
 datas = []
-datas += [("myenv/Lib/site-packages/streamlit/runtime", "./streamlit/runtime")]
-datas += collect_data_files("streamlit")
 datas += copy_metadata("streamlit")
 datas += copy_metadata("streamlit_plotly_events")
-datas += collect_data_files("st_pages")
 datas += copy_metadata("st_pages")
 datas += copy_metadata("pyopenms")
 datas += copy_metadata("captcha")
