4035d2add88a6da64e9a55662f780dfcd3da3570
Samuel Wein <sam@samwein.com>
2024-05-15 17:22:17 +0200

    add documentation

--- pages/14_TOPP-Workflow.py
+++ pages/14_TOPP-Workflow.py
@@ -43,36 +43,36 @@ if __name__ == "__main__":
                     
                       ### Prerequisites 
                     
-                      In order to search your own data 
+                      In order to search your own data you need two things: Output from a MS experiment formatted as an [mzML file](https://doi.org/10.1007%2F978-1-60761-444-9_22),
+                      and a set of sequences to search formatted as a [FASTA file](https://en.wikipedia.org/wiki/FASTA_format). You can convert the raw files produced by your instrument to mzML
+                      using [MSConvert](https://proteowizard.sourceforge.io/). Search sequences are formatted as a standard FASTA entries with sequences including the canonical RNA bases (A,U,C,G),
+                      as well as "short name" representations of modified residues from [Modomics](https://genesilico.pl/modomics/modifications) surrounded by square brackets (see below for an example)
+                    
+                      > **_Example:_** RNA sequence: AUCCCUGGACAUA[Am]CU[P]AAA - Includes 2'-O-methyladenosine and Pseudouridine, in addition to cannoncial bases
+                    
+                      Note that the FASTA file can contain many sequences, if you are searching for multiple forms or RNAs.
+                    
+                      ### Uploading your files
+                    
+                      Navigate to the "📁File Upload" tab in NASE-Weis. There are two sub-tabs "MS data" and "Nucleotide sequences". You can upload your mzML file under the MS-data sub-tab, and your FASTA file under the "Nucleotide sequences" sub-tab by clicking "Browse files",
+                      selecting your file, and then clicking  **Add MS data** or **Add nucleotide sequence file**. After uploading the files, you should see a "Current files" box showing the files that you have uploaded.
+                    
+                      ### Configuring NASE-Weis
 
-                      Clone the [streamlit-template repository](https://github.com/OpenMS/streamlit-template). It includes files to install dependencies via pip or conda.
+                      Navigate to the "⚙️ **Configure**" tab in NASE-Weis. Select the files that you uploaded in the previous step in the mzML-files and fasta-files boxes. If you don't want to do FDR calculations click on the "do FDR?" checkbox. You can also adjust the FDR cutoff below.
+                      Once you are satisfied with the FDR settings, click the "Nucleic acid search engine" tab above the "Do FDR?" box. Here you will see options for the search engine itself. The search engine automatically selects internal tolerance, and mass options based on the resolution of the data
+                      make sure that the settings for "MS1 approximate resolution?" and "MS2 approximate resolution?" **roughly** match those for your data. If the RNA was digested with an enzyme before the experiment, select your enzyme in the "enzyme box", and select the number of missed cleavages
+                      to allow. After you are happy with these settings, click the "Save Parameters" button above.
+                    
+                      ### Running NASE-Weis
+                    
+                      Navigate to the "🚀 **Run**" tab in NASE-Weis. Click the "Start Workflow" button. You will get a "WORKFLOW FINISHED" message below when the workflow is done. If you want to see more details, you can configure the "log details" above the start button.
+                    
+                      ### Viewing the results
 
-                      ### via pip in an existing Python environment
+                      Navigate to the 📊 **Results** tab. There you will see a table of the results of your search, showing RNA's were matched, what digestion fragment matched, the retention time and mass to charge ratio of the match, its charge, the search engine score, and (if FDR is enabled) the q-value in %.
+                      Below the table is a button to download it as a CSV. There are also buttons to download the idXML formatted hits (for use with pyOpenMS or TOPPView), and mztab formatted output.
 
-                      To install all required depdencies via pip in an already existing Python environment, run the following command in the terminal:
-
-                      `pip install -r requirements.txt`
-
-                      ### create new environment via conda/mamba
-
-                      Create and activate the conda environment:
-
-                      `conda env create -f environment.yml`
-
-                      `conda activate streamlit-env`
-
-                      ### run the app
-
-                      Run the app via streamlit command in the terminal with or without *local* mode (default is *online* mode). Learn more about *local* and *online* mode in the documentation page 📖 **OpenMS Template App**.
-
-                      `streamlit run app.py [local]`
-
-                      ## Docker
-
-                      This repository contains two Dockerfiles.
-
-                      1. `Dockerfile`: This Dockerfile builds all dependencies for the app including Python packages and the OpenMS TOPP tools. Recommended for more complex workflows where you want to use the OpenMS TOPP tools for instance with the **TOPP Workflow Framework**.
-                      2. `Dockerfile_simple`: This Dockerfile builds only the Python packages. Recommended for simple apps using pyOpenMS only.
 
                       """)
     with t[1]:
