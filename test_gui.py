97a4126f4af8b5e904333bb33434fdf782895d87
Joshua Charkow <joshuacharkow@gmail.com>
2024-12-05 20:35:24 -0500

    add test_file_upload

--- test_gui.py
+++ test_gui.py
@@ -11,6 +11,7 @@ def launch(request):
 
 
 
+# Test launching of all pages
 @pytest.mark.parametrize('launch', (
                        #"content/quickstart.py", # NOTE: this page does not work due to streamlit.errors.StreamlitPageNotFoundError error
                        "content/documentation.py",
@@ -41,3 +42,32 @@ def test_documentation(launch, selection):
     launch.run()
     launch.selectbox[0].select(selection).run()
     assert not launch.exception
+
+
+@pytest.mark.parametrize('launch,tab', [("content/file_upload.py", "File Upload"), 
+                                        ("content/file_upload.py", "Example Data"), 
+                                        ("content/file_upload.py", "Files from local folder")], indirect=['launch'])
+def test_file_upload(launch, tab):
+    launch.run()
+    print(launch.tabs)
+    for i in launch.tabs:
+        print(i.label)
+        if i.label == tab:
+            if i.label == "Files from local folder":
+                pass # This is a tkinter button so pass
+            else:
+                i.button[0].click().run()
+                assert not launch.exception
+
+
+##### IN PROGRESS
+@pytest.mark.parametrize('launch,tab', [("content/raw_data_viewer.py", "Peak map (MS1)"), 
+                                         ("content/raw_data_viewer.py", "Spectra (MS1 + MS2)"), 
+                                         ("content/raw_data_viewer.py", "Chromatograms (MS1)")], indirect=['launch'])
+def view_raw_ms_data(launch, tab):
+    launch.run()
+    for i in launch.tabs:
+        if i.label == tab:
+            i.button[0].click().run()
+            assert not launch.exception
+
