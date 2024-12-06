99c30a63c0507d5bd4b83fc142c00e5ac0b33afc
Joshua Charkow <joshuacharkow@gmail.com>
2024-12-06 13:12:52 -0500

    add more specific tests for run_workflow
    fix tests for view_ms_raw_data and file_upload

--- test_gui.py
+++ test_gui.py
@@ -1,13 +1,18 @@
 from streamlit.testing.v1 import AppTest
 import pytest
 from src import fileupload
-
+import json
+from pathlib import Path
+import shutil
 
 @pytest.fixture
 def launch(request):
     test = AppTest.from_file(request.param)
-    # disable online mode for testing
-    test.session_state['settings'] = {'online_deployment':False, 'google_analytics':{'enabled':False}}
+
+    ## Initialize session state ##
+    with open("settings.json", "r") as f:
+        test.session_state.settings = json.load(f)
+    test.secrets['workspace'] = 'test'
     return test
 
 
@@ -45,33 +50,48 @@ def test_documentation(launch, selection):
     assert not launch.exception
 
 
-@pytest.mark.parametrize('launch,tab', [("content/file_upload.py", "File Upload"), 
-                                        ("content/file_upload.py", "Example Data"), 
-                                        ("content/file_upload.py", "Files from local folder")], indirect=['launch'])
-def test_file_upload(launch, tab):
+@pytest.mark.parametrize('launch', ["content/file_upload.py"], indirect=True)
+def test_file_upload_load_example(launch):
     launch.run()
-    print(launch.tabs)
     for i in launch.tabs:
-        print(i.label)
-        if i.label == tab:
-            if i.label == "Files from local folder":
-                pass # This is a tkinter button so pass
-            else:
-                i.button[0].click().run()
-                assert not launch.exception
-
-
-##### IN PROGRESS
-@pytest.mark.parametrize('launch,tab', [("content/raw_data_viewer.py", "Peak map (MS1)"), 
-                                         ("content/raw_data_viewer.py", "Spectra (MS1 + MS2)"), 
-                                         ("content/raw_data_viewer.py", "Chromatograms (MS1)")], indirect=['launch'])
-def test_view_raw_ms_data(launch, tab):
-    ## Load Example file ##    
-    launch.run()
-    fileupload.load_example_mzML_files()
-
-    for i in launch.tabs:
-        if i.label == tab:
+        if i.label == "Example Data":
             i.button[0].click().run()
             assert not launch.exception
 
+
+# NOTE: All tabs are automatically checked
+@pytest.mark.parametrize('launch,example', [("content/raw_data_viewer.py", 'Blank.mzML'),
+                                            ("content/raw_data_viewer.py", 'Treatment.mzML'),
+                                            ("content/raw_data_viewer.py", 'Pool.mzML'),
+                                            ("content/raw_data_viewer.py", 'Control.mzML')], indirect=['launch'])
+def test_view_raw_ms_data(launch, example):
+    launch.run()
+    ## Load Example file, based on implementation of fileupload.load_example_mzML_files() ###
+    mzML_dir = Path(launch.session_state.workspace, "mzML-files")
+    # Copy files from example-data/mzML to workspace mzML directory, add to selected files
+    for f in Path("example-data", "mzML").glob("*.mzML"):
+        shutil.copy(f, mzML_dir)
+    launch.run()
+
+    ## Select file and check if it is displayed
+    launch.selectbox[0].select(example).run()
+    assert not launch.exception
+
+
+@pytest.mark.parametrize('launch,example', [("content/run_example_workflow.py", 'Blank'),
+                                            ("content/run_example_workflow.py", 'Treatment'),
+                                            ("content/run_example_workflow.py", 'Pool'),
+                                            ("content/run_example_workflow.py", 'Control')], indirect=['launch'])
+def test_run_workflow(launch, example):
+    launch.run()
+    ## Load Example file, based on implementation of fileupload.load_example_mzML_files() ###
+    mzML_dir = Path(launch.session_state.workspace, "mzML-files")
+    # Copy files from example-data/mzML to workspace mzML directory, add to selected files
+    for f in Path("example-data", "mzML").glob("*.mzML"):
+        shutil.copy(f, mzML_dir)
+    launch.run()
+
+    ## Select file and check if it is displayed
+    launch.multiselect[0].select(example)
+    launch.button[0].click().run()
+    assert not launch.exception
